2863f6eede70586a243148d5b3041ed38dc1fbc8
Faf-Desktop <jordivdh@hotmail.com>
2017-09-20 15:46:59 +0200

    Started with crouching

--- Gamelab-Jaar3-UnityProject/Assets/Sourcefiles/Scripts/Player/ExploreController.cs
+++ Gamelab-Jaar3-UnityProject/Assets/Sourcefiles/Scripts/Player/ExploreController.cs
@@ -4,33 +4,51 @@ using UnityEngine;
 
 public class ExploreController : MonoBehaviour
 {
-    public float hVelocity;
-    public float speed;
+    float hVelocity;
+    float speed;
+    float cameraHeight;
+    float vVelocity;
 
-    public float vVelocity;
-
-    public ExploreStats exploreStats;
+    ExploreStats exploreStats;
+    public Transform camTransform;
 
 
 	// Use this for initialization
 	void Awake()
     {
-        exploreStats = GameObject.Find("GameManager").GetComponent<ExploreStats>();	
+        exploreStats = GameObject.Find("GameManager").GetComponent<ExploreStats>();
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate ()
+	void Update ()
     {
-        //speed = (Input.GetKey(KeyCode.LeftShift)) ? exploreStats.walkSpeed : exploreStats.runSpeed;
-        speed = 2f;
-
-		if(PlayMode.gameMode == PlayMode.GameMode.Explore)
+        if (PlayMode.gameMode == PlayMode.GameMode.Explore)
         {
-            hVelocity = Input.GetAxis("Horizontal");
-            vVelocity = Input.GetAxis("Vertical");
-
-            Vector3 moveTowardsPos = new Vector3(hVelocity, 0, vVelocity);
-            transform.Translate(moveTowardsPos * speed * Time.deltaTime);
+            Move();
+            Crouch();
         }
 	}
+
+    void Move()
+    {
+        speed = Input.GetKey(KeyCode.LeftShift) ? exploreStats.runSpeed : exploreStats.walkSpeed;
+
+
+        hVelocity = Input.GetAxis("Horizontal");
+        vVelocity = Input.GetAxis("Vertical");
+
+        Vector3 moveTowardsPos = new Vector3(hVelocity, 0, vVelocity);
+        transform.Translate(moveTowardsPos * speed * Time.deltaTime);
+    }
+
+    void Crouch()
+    {
+        cameraHeight = Input.GetKey(KeyCode.LeftControl) ? -exploreStats.crouchDrop : 0;
+        Vector3 camPosition = new Vector3(0, cameraHeight, 0);
+
+        print(cameraHeight);
+
+        camTransform.localPosition = Vector3.Lerp(new Vector3(0, 1, 0), camPosition, exploreStats.crouchSpeed * Time.deltaTime);
+    }
+
 }
