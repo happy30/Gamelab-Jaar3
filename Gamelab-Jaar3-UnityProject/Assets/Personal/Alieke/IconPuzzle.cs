50eed8c02a5050ed74bedf9a55e240f7f0d820ea
Alieke <aliekevanderlelie@hotmail.com>
2017-09-27 15:32:05 +0200

    My scene + planning

---
+++ Gamelab-Jaar3-UnityProject/Assets/Personal/Alieke/IconPuzzle.cs
@@ -0,0 +1,53 @@
+﻿//Made by Alieke
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconPuzzle : MonoBehaviour
+{
+    public bool[] iconOrderArray;
+    public GameObject[] images;
+    private int nextImage = 0;
+	
+	public void CheckOrder (int number)
+    {
+        if (number == 2 && !iconOrderArray[3] && iconOrderArray[2])
+        {
+            iconOrderArray[3] = true;
+            ActiveImage();
+        }
+
+        if (iconOrderArray[3])
+        {
+            number++;
+        }
+ 
+        for (int i = 0; i <= number; i++)
+        {    
+            if(!iconOrderArray[i] && number == i)
+            {
+                iconOrderArray[i] = true;
+                ActiveImage();
+                if (iconOrderArray[4])
+                {
+                    print("puzzle completed");
+                    //complete puzzle
+                }
+                break;
+            }
+            else if(iconOrderArray[i] == false && number != i)
+            {
+                break;
+            }
+        }
+    }
+
+    void ActiveImage()
+    {
+        if (nextImage < images.Length)
+        {
+            images[nextImage].SetActive(true);
+            nextImage++;
+        }
+    }
+}
