756a93d89ca6189b919fd0bc20d72ba9bed96469
Jordi -happy30- <jordivdh@hotmail.com>
2017-09-29 12:48:56 +0200

    added interface puzzlecontroller

---
+++ Gamelab-Jaar3-UnityProject/Assets/Sourcefiles/Scripts/Puzzles/IPuzzleController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPuzzleController : MonoBehaviour
+{
+    public void Reset() { }
+    public void OnSolve() { }
+    public void OnFail() { }
+}
