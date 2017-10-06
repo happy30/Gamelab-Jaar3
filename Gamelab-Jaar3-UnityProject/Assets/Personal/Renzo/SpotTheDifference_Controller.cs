69a5076f585683e585f19138c062a8ccf7f79e3a
Renzo F <eagyr@hotmail.nl>
2017-10-06 12:45:48 +0200

    SpotTheDifference Script update Shitload aan meta's?

---
+++ Gamelab-Jaar3-UnityProject/Assets/Personal/Renzo/SpotTheDifference_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotTheDifference_Controller : MonoBehaviour {
+
+    //public List<GameObject> spots = new List<GameObject>();
+    public int totalSpots;
+    public int foundSpots;
+
+    public static SpotTheDifference_Controller spotController = null;
+
+	void Start () {
+        spotController = this;
+	}
+	
+	void Update () {
+		
+	}
+}
