628d8c2e3ed2730ead11beaf48ce3887c4a44c1c
Renzo F <eagyr@hotmail.nl>
2017-10-11 13:06:24 +0200

    Load aan Meta's, SpotTheDifference Update

--- Gamelab-Jaar3-UnityProject/Assets/Personal/Renzo/SpotTheDifferences.cs
+++ Gamelab-Jaar3-UnityProject/Assets/Personal/Renzo/SpotTheDifferences.cs
@@ -8,10 +8,19 @@ public class SpotTheDifferences : MonoBehaviour
     public int totalSpots;
     public int foundSpots;
     private bool isFound;
-    private ParticleSystem pS;
+    public float timer;
+    public float timerSetDuration;
+
 
     void Start ()
     {
+        timer = timerSetDuration;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            StartCoroutine(LevelTimer());
     }
 
     public void Clicker(Image img)
@@ -27,14 +36,26 @@ public class SpotTheDifferences : MonoBehaviour
 
     public IEnumerator ChangeAlpha(Image imgg)
     {
+        for (float f = 0f; f <= 2f; f += .1f)
         {
-            for (float f = 0f; f <= 2f; f += .1f)
-            {
-                Color tempColor = imgg.color;
-                tempColor.a = f;
-                imgg.color = tempColor;
-                yield return null;
-            }
+            Color tempColor = imgg.color;
+            tempColor.a = f;
+            imgg.color = tempColor;
+            yield return null;
         }
+    }
+
+    public IEnumerator LevelTimer()
+    {
+        while(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        // Crash the puzzle
+
+        timer = timerSetDuration;
+        yield break;
     }
 }
