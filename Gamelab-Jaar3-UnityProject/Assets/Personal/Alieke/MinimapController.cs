3c18ac99e2fa696da15f089aa09e0d818b404f11
Alieke <aliekevanderlelie@hotmail.com>
2017-12-18 13:03:02 +0100

    Minimap in progress, need to push for pull

---
+++ Gamelab-Jaar3-UnityProject/Assets/Personal/Alieke/MinimapController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapController : MonoBehaviour
+{
+    public Transform player;
+    public GameObject playerUI;
+
+	void LateUpdate ()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = player.position.y;
+        transform.position = newPosition;
+
+        playerUI.transform.eulerAngles = new Vector3(0, 0, -player.eulerAngles.y);
+	}
+}
